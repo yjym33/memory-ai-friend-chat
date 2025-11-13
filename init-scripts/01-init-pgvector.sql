64d17a8b802c0b02861047a00ef47d3f113319ca
yjym33 <yjym33@naver.com>
2025-11-13 20:52:20 +0900

    불필요한 파일들 제거

--- init-scripts/01-init-pgvector.sql
+++
@@ -1,8 +0,0 @@
--- pgvector 확장 설치
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- 벡터 검색을 위한 인덱스 타입 확인
-SELECT * FROM pg_available_extensions WHERE name = 'vector';
-
--- 임베딩 차원 확인 (OpenAI text-embedding-3-small은 1536차원)
--- 나중에 테이블 생성 시 vector(1536) 사용
